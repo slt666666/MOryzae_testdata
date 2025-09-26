59c6e7582a786de3cdc4db188ef5d67269f629ee
Toshiyuki Sakai <toshi6661024@gmail.com>
2025-09-27 01:46:06 +0900

    Create RNA_seq.sh

---
+++ RNA_seq.sh
@@ -0,0 +1,9 @@
+for i in infect_0h_rep1 infect_0h_rep2 infect_0h_rep3 infect_48h_rep1 infect_48h_rep2 infect_48h_rep3;do
+    wget -O ${i}.fastq.gz https://raw.githubusercontent.com/slt666666/MOryzae_testdata/refs/heads/main/${i}.fastq.gz
+    hisat2 -x genome_index -U ${i}.fastq.gz | samtools sort -O bam > ${i}.bam
+    samtools index ${i}.bam
+done
+
+stringtie *.bam -o transcripts.gtf
+
+featureCounts -a transcripts.gtf -o counts_0h_vs_48h.txt *.bam
